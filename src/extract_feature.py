771c66fab084975f6e27ffa579ced66b36e5de84
ramyasree9 <ramyasusanth@gmail.com>
2019-10-12 12:33:01 -0400

    renamed feature extraction methods

--- src/extract_feature.py
+++
@@ -1,4 +0,0 @@
-from extract_feature_hog import extract_feature_hog
-
-def extract_feature(grayscale_img):
-    return extract_feature_hog(grayscale_img)
---
+++ src/extract_features.py
@@ -0,0 +1,4 @@
+from extract_features_hog import extract_features_hog
+
+def extract_features(grayscale_img):
+    return extract_features_hog(grayscale_img)

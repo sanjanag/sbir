4853535f73a2a4bee1771c6ccc95db3f778b6cd3
ramyasree9 <ramyasusanth@gmail.com>
2019-10-31 17:30:38 -0400

    tuned hog parameter

--- src/feature_extraction/extract_features_hog.py
+++ src/feature_extraction/extract_features_hog.py
@@ -3,6 +3,6 @@ from skimage import feature
 
 def extract_features_hog(grayscale_img):
     histogram = feature.hog(grayscale_img, orientations=9,
-                            pixels_per_cell=(16, 16), cells_per_block=(2, 2),
+                            pixels_per_cell=(64, 64), cells_per_block=(2, 2),
                             transform_sqrt=True, block_norm="L1")
     return histogram
